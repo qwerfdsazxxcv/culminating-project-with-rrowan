70bac30d107ace38327415b3b849b45e587f354b
qwerfdsazxxcv <161515152+qwerfdsazxxcv@users.noreply.github.com>
2024-02-27 14:36:02 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,111 @@
+namespace SpriteKind {
+    export const Endpoint = SpriteKind.create()
+}
+function Stage () {
+    tiles.setCurrentTilemap(tilemap`level3`)
+}
+function doSomething (mySprite: Sprite, x: number, y: number) {
+    mySprite.setPosition(x, spawnpooint)
+    scene.cameraFollowSprite(mySprite)
+    controller.player2.moveSprite(Player2)
+}
+scene.onOverlapTile(SpriteKind.Player, assets.tile`transparency16`, function (sprite, location) {
+	
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Endpoint, function (sprite, otherSprite) {
+    info.stopCountdown()
+    p1time = info.countdown()
+    doSomething(Player2, 505, spawnpooint)
+    if (Variable == 1) {
+        Stage()
+    }
+    if (Variable == 2) {
+        Variable = -1
+    }
+    Variable += 1
+})
+let p1time = 0
+let Variable = 0
+let Player2: Sprite = null
+let spawnpooint = 0
+tiles.setCurrentTilemap(tilemap`level2`)
+let Player1 = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 2 2 2 2 2 2 e e f . . 
+    . . f e 2 f f f f f f 2 e f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f b f 4 4 f b f e f f . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(Player1)
+scene.cameraFollowSprite(Player1)
+spawnpooint = 251
+Player1.setPosition(12, spawnpooint)
+info.startCountdown(100)
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . 1 1 . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Endpoint)
+let endpoin2 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . 1 1 . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Endpoint)
+mySprite.setPosition(245, 5)
+endpoin2.setPosition(285, 5)
+Player2 = sprites.create(img`
+    . . . . . . 5 . 5 . . . . . . . 
+    . . . . . f 5 5 5 f f . . . . . 
+    . . . . f 1 5 2 5 1 6 f . . . . 
+    . . . f 1 6 6 6 6 6 1 6 f . . . 
+    . . . f 6 6 f f f f 6 1 f . . . 
+    . . . f 6 f f d d f f 6 f . . . 
+    . . f 6 f d f d d f d f 6 f . . 
+    . . f 6 f d 3 d d 3 d f 6 f . . 
+    . . f 6 6 f d d d d f 6 6 f . . 
+    . f 6 6 f 3 f f f f 3 f 6 6 f . 
+    . . f f d 3 5 3 3 5 3 d f f . . 
+    . . f d d f 3 5 5 3 f d d f . . 
+    . . . f f 3 3 3 3 3 3 f f . . . 
+    . . . f 3 3 5 3 3 5 3 3 f . . . 
+    . . . f f f f f f f f f f . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+Variable = 0
