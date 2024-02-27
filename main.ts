3fac95078f3302d4eaa0bb45a6a4e7cb6c2f0fc5
qwerfdsazxxcv <161515152+qwerfdsazxxcv@users.noreply.github.com>
2024-02-27 14:46:37 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,48 @@
 namespace SpriteKind {
     export const Endpoint = SpriteKind.create()
 }
-function Stage () {
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    Player1.throwDart()
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.builtin.brick, function (sprite, location) {
+    sprite.setVelocity(0, 0)
+})
+function Stage (mySprite: Sprite) {
     tiles.setCurrentTilemap(tilemap`level3`)
+    mySprite.setPosition(15, 205)
+    scene.cameraFollowSprite(mySprite)
+    Player1 = darts.create(img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f f e 2 2 2 2 2 2 e e f . . 
+        . . f e 2 f f f f f f 2 e f . . 
+        . . f f f f e e e e f f f f . . 
+        . f f e f b f 4 4 f b f e f f . 
+        . f e e 4 1 f d d f 1 4 e e f . 
+        . . f e e d d d d d d e e f . . 
+        . . . f e e 4 4 4 4 e e f . . . 
+        . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+        . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
+        `, SpriteKind.Player)
+    Player1.controlWithArrowKeys()
+    Player1.setTrace()
 }
 function doSomething (mySprite: Sprite, x: number, y: number) {
     mySprite.setPosition(x, spawnpooint)
     scene.cameraFollowSprite(mySprite)
     controller.player2.moveSprite(Player2)
 }
-scene.onOverlapTile(SpriteKind.Player, assets.tile`transparency16`, function (sprite, location) {
-	
-})
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Endpoint, function (sprite, otherSprite) {
     info.stopCountdown()
     p1time = info.countdown()
     doSomething(Player2, 505, spawnpooint)
     if (Variable == 1) {
-        Stage()
+        Stage(Player1)
     }
     if (Variable == 2) {
         Variable = -1
@@ -27,9 +52,11 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Endpoint, function (sprite, othe
 let p1time = 0
 let Variable = 0
 let Player2: Sprite = null
+let mySprite: Sprite = null
 let spawnpooint = 0
+let Player1: Dart = null
 tiles.setCurrentTilemap(tilemap`level2`)
-let Player1 = sprites.create(img`
+Player1 = sprites.create(img`
     . . . . . . f f f f . . . . . . 
     . . . . f f f 2 2 f f f . . . . 
     . . . f f f 2 2 2 2 f f f . . . 
@@ -52,7 +79,7 @@ scene.cameraFollowSprite(Player1)
 spawnpooint = 251
 Player1.setPosition(12, spawnpooint)
 info.startCountdown(100)
-let mySprite = sprites.create(img`
+mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
@@ -108,4 +135,4 @@ Player2 = sprites.create(img`
     . . . f f f f f f f f f f . . . 
     . . . . . f f . . f f . . . . . 
     `, SpriteKind.Player)
-Variable = 0
+Variable = -1
